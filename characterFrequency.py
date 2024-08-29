3e1cc705eaf5d5b48ba41cb2da55e853377693e8
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-29 21:00:09 +0300

    string frequency

--- characterFrequency.py
+++ characterFrequency.py
@@ -1,1 +1,12 @@
 # character frequency in a string
+
+def character_freq(word, character='a'):
+    frequency = 0
+    for ch in word:
+        if ch == character:
+            frequency += 1
+    return frequency
+
+word = input("Enter any word: ")
+
+print("The frequency of the character is:", character_freq(word, character='a'))
