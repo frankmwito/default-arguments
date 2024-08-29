c514624bd0048025e3b352de973923ea217dca14
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-29 21:18:40 +0300

    numbers in a range given the starting as the default argument = 1

--- range.py
+++ range.py
@@ -1,1 +1,7 @@
-# return a list of number in a range given the starting as the default argument = 1
+# return a list of numbers in a range given the starting as the default argument = 1
+
+def range_of(end, start = 1):
+    for i in range (start, end):
+        print(i)
+    
+print("Your range in numbers is", range_of(end= int(input("Enter the ending number: ")), start= 1))
